b213cb24cc3011dfe65bfda28160b112bc6cf54e
Haydee Rodriguez <herodriguez@nearsoft.com>
2018-03-30 12:27:15 -0600

    New branch with all changes for the merge

---
+++ MyMovies/Controllers/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  MyMovies
+//
+//  Created by Haydee Rodriguez on 3/20/18.
+//  Copyright © 2018 Claudia Carrillo. All rights reserved.
+//
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        AuthHandler.facebookLogin(loginVC: self)
+    }
+    
+    func changeView(with storyboardName: String, viewControllerName: String) {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: viewControllerName) as UIViewController
+        present(initialViewController, animated: true, completion: nil)
+    }
+}
