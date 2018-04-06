14d578da02b060f4b4426603095c9ace4f219697
Haydee Rodriguez <herodriguez@nearsoft.com>
2018-04-06 15:16:43 -0500

    Add category model

---
+++ MyMovies/Models/MovieCategory.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCategory.swift
+//  MyMovies
+//
+//  Created by Haydee Rodriguez on 4/6/18.
+//  Copyright © 2018 Claudia Carrillo. All rights reserved.
+//
+
+import Foundation
+
+class MovieCategory {
+    var name: String?
+    var movies: [Movie]?
+    
+}
---
+++ MyMovies/Views/CategoryCell.swift
@@ -0,0 +1,108 @@
+//
+//  CategoryCell.swift
+//  MyMovies
+//
+//  Created by Haydee Rodriguez on 4/6/18.
+//  Copyright © 2018 Claudia Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    var featuredAppsController: UICollectionViewController?
+    fileprivate let cellId = "movieCellId"
+    var movieCategory: MovieCategory? {
+        didSet {
+            if let name = movieCategory?.name {
+                nameLabel.text = name
+            }
+            appsCollectionView.reloadData()
+        }
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Best New Movies"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let appsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.4, alpha: 0.4)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    func setupViews() {
+        backgroundColor = UIColor.clear
+        
+        addSubview(appsCollectionView)
+        addSubview(dividerLineView)
+        addSubview(nameLabel)
+        
+        appsCollectionView.dataSource = self
+        appsCollectionView.delegate = self
+        
+        appsCollectionView.register(MovieCollectionCell.self, forCellWithReuseIdentifier: cellId)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": dividerLineView]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[nameLabel(30)][v0][v1(0.5)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView, "v1": dividerLineView, "nameLabel": nameLabel]))
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let count = movieCategory?.movies?.count {
+            return count
+        }
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MovieCollectionCell
+        cell.movies = movieCategory?.movies[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: frame.height - 32)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 14, 0, 14)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let app = appCategory?.apps?[indexPath.item] {
+            featuredAppsController?.showAppDetailForApp(app)
+        }
+        
+    }
+    
+}
